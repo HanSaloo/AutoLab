a9401168c05e5e98d3b98a2d73dca497bb4d2e1b
HanSaloo <55971365+HanSaloo@users.noreply.github.com>
2021-01-19 21:41:04 +0300

    new up
    add new fitures

--- src/com/company/Creator.java
+++ src/com/company/Creator.java
@@ -10,16 +10,24 @@ public class Creator{
 
         System.out.println("Hello! It a program for automatic crate a labs files.");
         Scanner in = new Scanner(System.in);
-        System.out.println("Please, input a count of labs: ");
+
+        System.out.print("Type name of your files: ");
+        String name = in.nextLine();
+
+        System.out.print("Input type of files (any, for example .txt or .xml): ");
+        String type = in.nextLine();
+
+        System.out.print("Please, input a count of labs: ");
         int labCount = in.nextInt();
+
         new File("/Users/admin/Desktop/AutoLab").mkdirs();
 
         for (int i = 1; i <= labCount; i++) {
-            File lFile = new File("/Users/admin/Desktop/AutoLab/Lab " + i + ".docx");
-            try {
+         File lFile = new File("/Users/admin/Desktop/AutoLab/"+ name +" " + i + type);
+         try {
                 boolean created = lFile.createNewFile();
                 if (created)
-                    System.out.println("File number " + i + " has been created");
+                    System.out.println("File number " + i + " with name"+ name +" and "+ type +" type, has been created!");
             } catch (IOException ex) {
 
                 System.out.println(ex.getMessage());
